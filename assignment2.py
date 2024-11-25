d2b72c80b056f945177f1a206db6f7982bbcc86a
Lucas <lucasmaranho21@gmail.com>
2024-11-25 16:51:47 +0000

    first commit defining the welcom function

---
+++ assignment2.py
@@ -0,0 +1,10 @@
+def welcome():
+    print("WELCOME TO BLOOD TYPE")
+    print("Check which blood types can donate to a given blood type: 1")
+    print("Check which blood types can receive donations from a given blood type: 2")
+    print("Exit the program: 3")
+    
+welcome()
+
+
+    
