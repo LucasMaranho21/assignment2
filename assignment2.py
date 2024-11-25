03a8b589e8a9a50ac0430ddf955feca607035e84
Lucas <lucasmaranho21@gmail.com>
2024-11-25 17:55:02 +0000

    dictionary to save data information about blood compatibility

--- assignment2.py
+++ assignment2.py
@@ -1,3 +1,4 @@
+#defining a welcome function. so it will print: welcome and menu options.
 def welcome():
     print("WELCOME TO BLOOD TYPE")
     print("Check which blood types can donate to a given blood type: 1")
@@ -6,5 +7,31 @@ def welcome():
     
 welcome()
 
-
+#
+blood_comp = {
+    "A+": {"gives": ["A+", "AB+"], 
+          "receives": ["A+", "A-", "O+", "O-"]},
     
+    "A-": {"gives": ["A+", "A-", "AB+", "AB-"], 
+           "receives": ["A-", "O-"]},
+        
+    "AB+": {"gives": ["AB+"], 
+            "receives": ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"]},
+
+    "AB-": {"gives": ["AB+", "AB-"], 
+            "receives": ["AB-", "A-", "B-", "O-"]},
+
+    "B+": {"gives": ["B+", "AB+"], 
+           "receives": ["B+", "B-", "O+", "O-"]},
+
+    "B-": {"gives": ["B+", "B-", "AB+", "AB-"], 
+           "receives": ["B-", "O-"]},
+
+    "O+": {"gives": ["O+", "A+", "B+", "AB+"], 
+           "receives": ["O+", "O-"]},
+
+    "O-": {"gives": ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"], 
+           "receives": ["O-"]}
+
+}
+
