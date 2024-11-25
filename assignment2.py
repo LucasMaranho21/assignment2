3ba4360c0141b4fcbb85482ee625818fcc991d8e
Lucas <lucasmaranho21@gmail.com>
2024-11-25 19:19:25 +0000

    main function to run the system

--- assignment2.py
+++ assignment2.py
@@ -1,11 +1,12 @@
 #defining a welcome function. so it will print: welcome and menu options.
 def welcome():
+    print("="*40)
     print("WELCOME TO BLOOD TYPE")
-    print("Check which blood types can donate to a given blood type: 1")
-    print("Check which blood types can receive donations from a given blood type: 2")
-    print("Exit the program: 3")
-    
-welcome()
+    print("="*40)
+    print("1. Check which blood types can donate to a given blood type: ")
+    print("2. Check which blood types can receive donations from a given blood type: ")
+    print("3. Exit the program: ")
+
 
 # Dictionary to save data information about blood compatibility.
 blood_compatibility = {
@@ -34,10 +35,54 @@ blood_compatibility = {
            "receives": ["O-"]}
 
 }
-
+#validation function for blood type.
 def blood_type_validation(blood):
     if blood in blood_compatibility:
         return True
     else:
         print(f"Blood type {blood} does not exist. Please type again.")
-        return False    
+        return False  
+
+def menu_validation_choice(menu_choice):
+    if menu_choice in ["1","2","3"]:
+        return True
+    else:
+        print(f"menu choice is not recognized. Please type 1, 2 or 3 only")
+        return False
+
+#function to bring the gives blood from the blood compatibility
+def get_gives(blood):
+    return blood_compatibility[blood]["gives"]
+
+#function to bring the receives blood from the blood compatibility
+def get_receives(blood):
+    return blood_compatibility[blood]["receives"]
+
+# main function to run the system.
+def main():
+    while True:
+        welcome()
+        menu_choice = input("Type 1,2,3: ").strip()
+        
+        if not menu_validation_choice(menu_choice):
+            continue
+        
+        if menu_choice == "3":
+            print("bye")
+            break
+
+        blood = input("blood type (e.g., A+, O-, AB+): ")
+        
+        if not blood_type_validation(blood):
+            continue
+        
+        if menu_choice == "1":
+            gives = get_gives(blood)
+            print(f"Blood Type {blood} can donate to {gives}" )
+        
+        if menu_choice == "2":
+            receives = get_receives(blood)
+            print(f"Blood Type {blood} can receive donations from: {receives}")
+
+if __name__ == "__main__":
+    main()
